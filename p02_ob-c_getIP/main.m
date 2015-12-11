50f9c67cd219593b68b6539e8ca707cbbdc2815a
matrix01 <milon06@gmail.com>
2015-12-12 00:59:58 +0600

    Added CPULoad and Ramuses

--- p02_ob-c_getIP/main.m
+++ p02_ob-c_getIP/main.m
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
+//System Network info related
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,9 +15,64 @@
 #include <net/if.h>
 #include <netdb.h>
 
+//Uses related
 #include <sys/sysctl.h>
 #include <netinet/in.h>
 #include <net/route.h>
+
+//System memory related
+#include <mach/vm_statistics.h>
+#include <mach/mach_types.h>
+#include <mach/mach_init.h>
+#include <mach/mach_host.h>
+
+static unsigned long long _previousTotalTicks = 0;
+static unsigned long long _previousIdleTicks = 0;
+
+// Returns 1.0f for "CPU fully pinned", 0.0f for "CPU idle", or somewhere in between
+// You'll need to call this at regular intervals, since it measures the load between
+// the previous call and the current one.
+float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks) {
+    unsigned long long totalTicksSinceLastTime = totalTicks-_previousTotalTicks;
+    unsigned long long idleTicksSinceLastTime  = idleTicks-_previousIdleTicks;
+    float ret = 1.0f-((totalTicksSinceLastTime > 0) ? ((float)idleTicksSinceLastTime)/totalTicksSinceLastTime : 0);
+    _previousTotalTicks = totalTicks;
+    _previousIdleTicks  = idleTicks;
+    return ret;
+}
+
+float getCPULoad() {
+    host_cpu_load_info_data_t cpuinfo;
+    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+    if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
+        unsigned long long totalTicks = 0;
+        for(int i=0; i<CPU_STATE_MAX; i++) totalTicks += cpuinfo.cpu_ticks[i];
+        float sysLoadPercentage = CalculateCPULoad(cpuinfo.cpu_ticks[CPU_STATE_IDLE], totalTicks);
+        printf("sysLoadPercentage: %f\n", sysLoadPercentage);
+        return 1;
+    }
+    else return -1.0f;
+}
+
+void getRamUses() {
+    vm_size_t page_size;
+    mach_port_t mach_port;
+    mach_msg_type_number_t count;
+    vm_statistics64_data_t vm_stats;
+    
+    mach_port = mach_host_self();
+    count = sizeof(vm_stats) / sizeof(natural_t);
+    if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
+        KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO,
+                                          (host_info64_t)&vm_stats, &count)) {
+            long long free_memory = (int64_t)vm_stats.free_count * (int64_t)page_size;
+            
+            long long used_memory = ((int64_t)vm_stats.active_count +
+                                     (int64_t)vm_stats.inactive_count +
+                                     (int64_t)vm_stats.wire_count) *  (int64_t)page_size;
+            printf("free memory: %lld\nused memory: %lld\n", free_memory, used_memory);
+        }
+}
 
 void getNetworkInfo() {
     NSLog(@"\nMACOSX - System Info Extraction Program!\n");
@@ -89,12 +144,14 @@ void getNetworkUses() {
             totalobytes += if2m->ifm_data.ifi_obytes;
         }
     }
-    printf("total ibytes %qu\tobytes %qu\n", totalibytes, totalobytes);
+    printf("Total ibytes %qu\tobytes %qu\n", totalibytes, totalobytes);
 }
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         getNetworkInfo();
         getNetworkUses();
+        getCPULoad();
+        getRamUses();
     }
     return 0;
 }
